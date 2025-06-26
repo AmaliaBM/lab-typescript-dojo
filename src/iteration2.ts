768c3af874f01b4195b131c2a6191f95086a789f
amalia barrigas <amaliabarrigas@gmail.com>
2025-06-26 10:54:35 +0200

    ejercicio terminado sin bonus

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -1,5 +1,21 @@
-//
-// Iteration 2 | Functions
-//
 
+function calcMultiplication(a: number, b: number): number {
+    return a*b;
+}
 
+function isEven(n: number): boolean {
+    return n%2 === 0;
+}
+
+function calcArrayAverage(numbersArr: number[]): string | number {
+    if (numbersArr.length === 0) {
+        return 'Please provide an array with at least one element';
+    }
+     const total = numbersArr.reduce((accumulator, currentValue) => {
+          return accumulator + currentValue;
+      });
+
+      const average = total / numbersArr.length;
+
+      return average;
+  }
--- src/iteration3.ts
+++ src/iteration3.ts
@@ -1,5 +1,12 @@
-//
-// Iteration 3 | Classes
-//
+  class BankAccount {
+      balance: number = 0;
+      accountHolder: string;
 
+      constructor(accountHolder: string){
+          this.accountHolder = accountHolder;
+      }
 
+      getBalance(): number{
+          return this.balance;
+      }
+  }  
